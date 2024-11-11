1e2a4ea2882634dfc2d8e55863558ea1dbd07f3d
blackcho72 <blackcho72@localhost>
2024-11-11 21:28:35 +0900

    GET 방식으로 사번을 리턴하는 API

--- src/main/java/com/example/demo/controller/UserNoController.java
+++ src/main/java/com/example/demo/controller/UserNoController.java
@@ -1,24 +1,19 @@
 package com.example.demo.controller;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.transform.sax.SAXResult;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 public class UserNoController {
 	   @GetMapping("/api/v1/user")
 	    public Map<String, String> getUserNo(HttpServletRequest request) {
 
-	        // 사번이 포함된 도메인 네임(ex: [사번].ce-aa.kubepia.net)
-	        String serverName = request.getServerName();
-
-	        // 도메인네임에서 사번만 추출
-	        String userNo = serverName.split("\\.")[0];
+	        String userNo = "82022592";
 
 	        Map<String, String> response = new HashMap<>();
-	        response.put("userNo", "222");
+	        response.put("userNo", userNo);
 	        return response;
 	    }
 }
